114a332f152c1a925a39bbe4c019242dbe0c9d8a
Damien Miller <djm@mindrot.org>
2024-10-03 02:16:02 +1000

    allow getsockopt in seccomp sandbox

--- sandbox-seccomp-filter.c
+++ sandbox-seccomp-filter.c
@@ -398,8 +398,12 @@ static const struct sock_filter preauth_insns[] = {
 #ifdef __NR_writev
 	SC_ALLOW(__NR_writev),
 #endif
+#ifdef __NR_getsockopt
+	SC_ALLOW(__NR_getsockopt),
+#endif
 #ifdef __NR_socketcall
 	SC_ALLOW_ARG(__NR_socketcall, 0, SYS_SHUTDOWN),
+	SC_ALLOW_ARG(__NR_socketcall, 0, SYS_GETSOCKOPT),
 	SC_DENY(__NR_socketcall, EACCES),
 #endif
 #if defined(__NR_ioctl) && defined(__s390__)
